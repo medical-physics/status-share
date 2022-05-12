274aefa538a6b438e393a85aaf561615db5455a5
James Chung <sunhochung53@gmail.com>
2022-05-12 03:11:58 -0700

    Use http to create server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+const http = require("http");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const dbo = require("./db/conn");
+
 app.use(cors());
 app.use(bodyParser.json());
 
-const dbo = require("./db/conn");
+const server = http.createServer(app);
 
-app.listen(port, () => {
+server.listen(port, () => {
 	dbo.connectToServer(function (err) {
 		if (err) console.error(err);
 	});
