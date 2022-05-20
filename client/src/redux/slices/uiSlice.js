bfe6902d05119f02c9d6ca7df7e68a4357e40d82
James Chung <sunhochung53@gmail.com>
2022-05-19 21:04:24 -0700

    Configure UI reducer

---
+++ client/src/redux/slices/uiSlice.js
@@ -0,0 +1,54 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    loading: false,
+    loadingTeam: false,
+    loadingUser: false,
+    errors: null
+};
+
+export const uiSlice = createSlice({
+    name: "ui",
+    initialState,
+    reducers: {
+        setErrors: (state, action) => {
+            state.loading = false;
+            state.errors = action.payload;
+        },
+        clearErrors: (state) => {
+            state.loading = false;
+            state.errors = null;
+        },
+        loadingUI: (state) => {
+            state.loading = true;
+        },
+        stopLoadingUI: (state) => {
+            state.loading = false;
+        },
+        loadingTeam: (state) => {
+            state.loadingTeam = true;
+        },
+        stopLoadingTeam: (state) => {
+            state.loadingTeam = false;
+        },
+        loadingUser: (state) => {
+            state.loadingUser = true;
+        },
+        stopLoadingUser: (state) => {
+            state.loadingUser = false;
+        }
+    }
+});
+
+export const {
+    setErrors,
+    clearErrors,
+    loadingUI,
+    stopLoadingUI,
+    loadingTeam,
+    stopLoadingTeam,
+    loadingUser,
+    stopLoadingUser
+} = uiSlice.actions;
+
+export default uiSlice.reducer;
