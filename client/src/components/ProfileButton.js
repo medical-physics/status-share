ef4c41e3dae00fbf7d0caf5b9223d6c58fd30821
schung53 <sunhochung53@gmail.com>
2022-07-08 13:24:04 -0400

    Refactor

--- client/src/components/ProfileButton.js
+++ client/src/components/ProfileButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 // MUI components
@@ -11,28 +11,26 @@ import {
   Notifications as NotificationsIcon
 } from '@mui/icons-material';
 
-export class ProfileButton extends Component {
-  render () {
-    const { onClick, unreadMessages } = this.props;
-    const tooltipString = 'Unread messages: ' + unreadMessages;
+export default function ProfileButton (props) {
+  const { onClick, unreadMessages } = props;
+  const tooltipString = 'Unread messages: ' + unreadMessages;
 
-    if (unreadMessages > 0) {
-      return (
-        <Tooltip title={tooltipString} arrow>
-          <IconButton onClick={onClick} style={{ color: '#b4004e' }} size='small'>
-            <NotificationsIcon />
-          </IconButton>
-        </Tooltip>
-      );
-    } else {
-      return (
-        <Tooltip title={tooltipString} arrow>
-          <IconButton onClick={onClick} size='small'>
-            <AccountCircleIcon />
-          </IconButton>
-        </Tooltip>
-      );
-    }
+  if (unreadMessages > 0) {
+    return (
+      <Tooltip title={tooltipString} arrow>
+        <IconButton onClick={onClick} style={{ color: '#b4004e' }} size='small'>
+          <NotificationsIcon />
+        </IconButton>
+      </Tooltip>
+    );
+  } else {
+    return (
+      <Tooltip title={tooltipString} arrow>
+        <IconButton onClick={onClick} size='small'>
+          <AccountCircleIcon />
+        </IconButton>
+      </Tooltip>
+    );
   }
 }
 
@@ -40,5 +38,3 @@ ProfileButton.propTypes = {
   onClick: PropTypes.func.isRequired,
   unreadMessages: PropTypes.number.isRequired
 };
-
-export default ProfileButton;
