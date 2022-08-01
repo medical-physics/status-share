cdfd374615c950b6789974461bdb31e696b34788
schung53 <sunhochung53@gmail.com>
2022-08-01 12:46:09 -0400

    Update private route condition

--- client/src/util/PrivateRoute.js
+++ client/src/util/PrivateRoute.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRoute = () => {
-  const authenticated = useSelector((state) => state.account.authenticated);
-  const checkingAuth = useSelector((state) => state.account.checkingAuth);
-
-  return (checkingAuth === false && authenticated === false && !localStorage.getItem('refreshToken'))
+  // Condition for logging users out immediately once session has ended
+  return (!localStorage.getItem('refreshToken'))
     ? <Navigate to='/login' />
     : <Outlet />;
 };
