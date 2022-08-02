5696dac5d3ae1797df8277c14d455aed2c0a540b
schung53 <sunhochung53@gmail.com>
2022-08-01 23:42:25 -0400

    Heroku build fix'

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const buildPath = path.join(__dirname, '..', 'client', 'build');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(buildPath));
-  app.length('/', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
