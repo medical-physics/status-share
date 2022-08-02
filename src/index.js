779b84c7ff0bc8d2691f2d120b29f56e436dc323
schung53 <sunhochung53@gmail.com>
2022-08-02 00:09:44 -0400

    Register login path for serving client

--- src/index.js
+++ src/index.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
+  app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
 }
 
 /* const options = {
